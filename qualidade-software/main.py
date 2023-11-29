f4c64ebc791e9f077b87c9fa52e52fde7ff51307
Juan Belieni <juanbelieni@gmail.com>
2023-11-29 20:48:19 -0300

    Add tests

--- qualidade-software/main.py
+++ qualidade-software/main.py
@@ -11,29 +11,39 @@ class CFPValidator(Validator):
     def __standardize_cpf(self, cpf):
         return re.sub(r"[^0-9]", "", cpf)
 
-    def __sum_of_products(self, cpf, digit: int):
-        sum_of_products, i = 0, 9 + digit
+    def __verify_if_all_equal(self, cpf: str):
+        first_digit = cpf[0]
+        return all(digit == first_digit for digit in cpf)
 
-        for number in cpf[:8+digit]:
+    def __sum_of_products(self, cpf: str, verifier_digit: int):
+        sum_of_products, i = 0, 9 + verifier_digit
+
+        for number in cpf[:8+verifier_digit]:
             sum_of_products += int(number) * i
             i -= 1
 
         return sum_of_products
 
-    def __expected_digit(self, cpf: str, digit: int):
-        mod = self.__sum_of_products(cpf, digit) % 11
+    def __expected_digit(self, cpf: str, verifier_digit: int):
+        mod = self.__sum_of_products(cpf, verifier_digit) % 11
         return 0 if mod < 2 else 11 - mod
 
     def validate(self, cpf: str):
         cpf = self.__standardize_cpf(cpf)
 
-        digit_1 = int(cpf[9])
-        digit_2 = int(cpf[10])
+        if len(cpf) != 11:
+            return False
 
-        expected_digit_1 = self.__expected_digit(cpf, 1)
-        expected_digit_2 = self.__expected_digit(cpf, 2)
+        if self.__verify_if_all_equal(cpf):
+            return False
 
-        return expected_digit_1 == digit_1 and expected_digit_2 == digit_2
+        verifier_digit_1 = int(cpf[9])
+        verifier_digit_2 = int(cpf[10])
+
+        expected_1 = self.__expected_digit(cpf, 1)
+        expected_2 = self.__expected_digit(cpf, 2)
+
+        return expected_1 == verifier_digit_1 and expected_2 == verifier_digit_2
 
 
 if __name__ == "__main__":
