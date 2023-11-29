b995427d3fba3c8c6564a0dc640542900de69d8f
Juan Belieni <juanbelieni@gmail.com>
2023-11-29 16:54:51 -0300

    Creating a validator interface

--- qualidade-software/main.py
+++ qualidade-software/main.py
@@ -1,79 +1,42 @@
 import re
+from abc import ABC, abstractmethod
 
-class CFPValidator:
-    def __init__(self, cpf):
-        self.cpf = self._standardize_cpf(cpf)
-    
-    def _standardize_cpf(self, cpf):
+
+class Validator(ABC):
+    @abstractmethod
+    def validate(self, value) -> bool: ...
+
+
+class CFPValidator(Validator):
+    def __standardize_cpf(self, cpf):
         return re.sub(r"[^0-9]", "", cpf)
-    
-    def _sum_of_products(self, cpf, digit:int=1):
+
+    def __sum_of_products(self, cpf, digit: int):
         sum_of_products, i = 0, 9 + digit
+
         for number in cpf[:8+digit]:
-            sum_of_products += int(number) * i 
+            sum_of_products += int(number) * i
             i -= 1
-            print(sum_of_products)
+
         return sum_of_products
-    
-    def _expected_digits(self, cpf, digit:int=1):
-        mod = self._sum_of_products(cpf, digit) % 11
-        print(mod)
-        print(0 if mod < 2 else 11 - mod)
+
+    def __expected_digit(self, cpf: str, digit: int):
+        mod = self.__sum_of_products(cpf, digit) % 11
         return 0 if mod < 2 else 11 - mod
-    
-    def validator_digit(self):
-        if self._expected_digits(self.cpf) == int(self.cpf[9]) and self._expected_digits(self.cpf, 2) == int(self.cpf[10]):
-            return True
-        else:
-            return False
-    
+
+    def validate(self, cpf: str):
+        cpf = self.__standardize_cpf(cpf)
+
+        digit_1 = int(cpf[9])
+        digit_2 = int(cpf[10])
+
+        expected_digit_1 = self.__expected_digit(cpf, 1)
+        expected_digit_2 = self.__expected_digit(cpf, 2)
+
+        return expected_digit_1 == digit_1 and expected_digit_2 == digit_2
+
+
 if __name__ == "__main__":
     cpf = input("Formato para inserir CPF - XXX.XXX.XXX-XX: ")
-    cpf_validator = CFPValidator(cpf)
-    print(cpf_validator.validator_digit())
-
-
-"""import re
-
-cpf_fornecido = input("Formato para inserir CPF - XXX.XXX.XXX-XX: ")
-cpf_tratado = re.sub(r"[^0-9]", "", cpf_fornecido)
-
-if cpf_tratado == cpf_tratado[0] * len(cpf_tratado):
-    print("CPF inválido")
-else:
-    cpf_sem_digitos_verificadores = cpf_tratado[:9]
-    nr_digitos = 10
-   
-    digito_calculado = 0
-   
-    for cada_digito in cpf_sem_digitos_verificadores:
-        digito_calculado += int(cada_digito) * nr_digitos
-        nr_digitos -= 1
-   
-    digito_verificador_1 = (digito_calculado * 10) % 11
-   
-    if digito_verificador_1 <= 9:
-        digito_verificador_1 = digito_verificador_1
-    else:
-        digito_verificador_1 = 0
-   
-    cpf_com_um_digito_verificador = cpf_sem_digitos_verificadores + str(digito_verificador_1)
-    nr_digitos = 11
-   
-    digito_calculado = 0
-   
-    for cada_digito in cpf_com_um_digito_verificador:
-        digito_calculado += int(cada_digito) * nr_digitos
-        nr_digitos -= 1
-   
-    digito_verificador_2 = (digito_calculado * 10) % 11
-   
-    if digito_verificador_2 <= 9:
-        digito_verificador_2 = digito_verificador_2
-    else:
-        digito_verificador_2 = 0
-   
-    if (int(cpf_tratado[9]) == digito_verificador_1) and (int(cpf_tratado[10]) == digito_verificador_2):
-        print("CPF é válido")
-    else:
-        print("CPF inválido")"""
+    cpf_validator = CFPValidator()
+    print(cpf_validator.validate(cpf))
