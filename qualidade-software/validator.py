cceb0394f7d0319957698704114e4ea877314cb2
Amanda Perez <nandaperez2002@gmail.com>
2023-11-29 21:47:00 -0300

    moving validator classes to other file

---
+++ qualidade-software/validator.py
@@ -0,0 +1,46 @@
+import re
+from abc import ABC, abstractmethod
+
+
+class Validator(ABC):
+    @abstractmethod
+    def validate(self, value) -> bool: ...
+
+
+class CPFValidator(Validator):
+    def __standardize_cpf(self, cpf):
+        return re.sub(r"[^0-9]", "", cpf)
+
+    def __verify_if_all_equal(self, cpf: str):
+        first_digit = cpf[0]
+        return all(digit == first_digit for digit in cpf)
+
+    def __sum_of_products(self, cpf: str, verifier_digit: int):
+        sum_of_products, i = 0, 9 + verifier_digit
+
+        for number in cpf[:8+verifier_digit]:
+            sum_of_products += int(number) * i
+            i -= 1
+
+        return sum_of_products
+
+    def __expected_digit(self, cpf: str, verifier_digit: int):
+        mod = self.__sum_of_products(cpf, verifier_digit) % 11
+        return 0 if mod < 2 else 11 - mod
+
+    def validate(self, cpf: str):
+        cpf = self.__standardize_cpf(cpf)
+
+        if len(cpf) != 11:
+            return False
+
+        if self.__verify_if_all_equal(cpf):
+            return False
+
+        verifier_digit_1 = int(cpf[9])
+        verifier_digit_2 = int(cpf[10])
+
+        expected_1 = self.__expected_digit(cpf, 1)
+        expected_2 = self.__expected_digit(cpf, 2)
+
+        return expected_1 == verifier_digit_1 and expected_2 == verifier_digit_2
