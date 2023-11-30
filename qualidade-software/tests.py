3c1589bb9c2bb13f77e28b804253ee92a37f7c3c
Juan Belieni <juanbelieni@gmail.com>
2023-11-29 21:40:55 -0300

    Fix tests

--- qualidade-software/tests.py
+++ qualidade-software/tests.py
@@ -1,43 +1,43 @@
-from main import CFPValidator
+from main import CPFValidator
 
 
 def test_valid_cpf():
-    cpf_validator = CFPValidator()
+    cpf_validator = CPFValidator()
     assert cpf_validator.validate("123.456.789-09")
 
 
 def test_invalid_cpf_invalid_verifier_digit_1():
-    cpf_validator = CFPValidator()
+    cpf_validator = CPFValidator()
     assert not cpf_validator.validate("123.456.789-08")
 
 
 def test_invalid_cpf_invalid_verifier_digit_2():
-    cpf_validator = CFPValidator()
+    cpf_validator = CPFValidator()
     assert not cpf_validator.validate("123.456.789-00")
 
 
 def test_valid_cpf_with_whitespace():
-    cpf_validator = CFPValidator()
+    cpf_validator = CPFValidator()
     assert cpf_validator.validate(" 123.456.789-09 ")
 
 
 def test_valid_cpf_only_digits():
-    cpf_validator = CFPValidator()
+    cpf_validator = CPFValidator()
     assert cpf_validator.validate("12345678909")
 
 
 def test_invalid_cpf_with_letters():
-    cpf_validator = CFPValidator()
+    cpf_validator = CPFValidator()
     assert not cpf_validator.validate("A23.456.789-09")
 
 
 def test_invalid_cpf_with_special_characters():
-    cpf_validator = CFPValidator()
+    cpf_validator = CPFValidator()
     assert not cpf_validator.validate("123.456.789-@#")
 
 
 def test_invalid_cpf_all_equal():
-    cpf_validator = CFPValidator()
+    cpf_validator = CPFValidator()
 
     for n in range(0, 10):
         assert not cpf_validator.validate(str(n) * 11)
