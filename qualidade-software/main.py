9d964f5a6a1a9aa5c7cf1bea7425527f8317e2f9
Amanda Perez <nandaperez2002@gmail.com>
2023-11-29 21:41:23 -0300

    changing print message

--- qualidade-software/main.py
+++ qualidade-software/main.py
@@ -49,4 +49,7 @@ class CPFValidator(Validator):
 if __name__ == "__main__":
     cpf = input("Formato para inserir CPF - XXX.XXX.XXX-XX: ")
     cpf_validator = CPFValidator()
-    print(cpf_validator.validate(cpf))
+    if cpf_validator.validate(cpf):
+        print("CPF é válido")
+    else:
+        print("CPF inválido")
